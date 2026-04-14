5a8872129ec9361caaee03901151ec0fdc2dc61d
Mysticz <salmanlimbada86@gmail.com>
2026-04-13 20:32:58 -0400

    installed some dependencies

--- server/index.js
+++ server/index.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+const http = require('http')
+const { Server } = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server, { cors: { origin: '*' } })
 const PORT = 3000
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../client/dist')))
 
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'server is running' })
+app.get('/api/questions', async (req, res) => {
+  const response = await fetch('https://opentdb.com/api.php?amount=10&type=multiple')
+  const data = await response.json()
+  res.json(data.results)
 })
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected:', socket.id)
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected:', socket.id)
+  })
+})
+
+server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
